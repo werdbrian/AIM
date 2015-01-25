a1201f2e6fefc5e9329aab44f3be6836fbc6e64a
imsosharp <imsosharp@mail.com>
2015-01-26 00:00:32 +0200

    fug :DDDDD

--- Autoplay/Behaviors/Positioning/Sequences.cs
+++ Autoplay/Behaviors/Positioning/Sequences.cs
@@ -10,7 +10,7 @@ namespace AIM.Autoplay.Behaviors.Positioning
     internal class Sequences
     {
         internal static Sequence LanePush = new Sequence(Actions.PushLane, Conditionals.ShouldPushLane, Inverters.LowHealth);
-        internal static Sequence StayWithinExpRange = new Sequence(Actions.StayWithinExpRange, Conditionals.ShouldPlaySafe);
+        internal static Sequence StayWithinExpRange = new Sequence(Actions.StayWithinExpRange);
         internal static Sequence CollectHealthPack = new Sequence(Actions.CollectHealthRelic, Conditionals.ShouldCollectHealthRelic);
         internal static Sequence TryToKill = new Sequence(Actions.KillEnemy);
     }
