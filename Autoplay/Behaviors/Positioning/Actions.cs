6c3e351121c28ace9c7064162524f5408017e612
imsosharp <imsosharp@mail.com>
2015-01-26 20:25:31 +0200

    wow?

--- Autoplay/Behaviors/Positioning/Actions.cs
+++ Autoplay/Behaviors/Positioning/Actions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AIM.Autoplay.Modes;
 using AIM.Autoplay.Util;
 using AIM.Autoplay.Util.Data;
 using AIM.Autoplay.Util.Objects;
@@ -58,7 +59,7 @@ namespace AIM.Autoplay.Behaviors.Positioning
                     var orbwalkingPos = new Vector2();
                     orbwalkingPos.X = ObjectManager.Player.ServerPosition.X + objConstants.DefensiveAdditioner;
                     orbwalkingPos.Y = ObjectManager.Player.ServerPosition.Y + objConstants.DefensiveAdditioner;
-                    ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, orbwalkingPos.To3D());
+                    Base.OrbW.ExecuteMixedMode(orbwalkingPos.To3D());
                     return BehaviorState.Success;
                 }
                 if (Heroes.Me.Distance(Modes.Base.ClosestEnemyMinion) < 750 ||
@@ -67,9 +68,11 @@ namespace AIM.Autoplay.Behaviors.Positioning
                     var orbwalkingPos = new Vector2();
                     orbwalkingPos.X = ObjectManager.Player.ServerPosition.X + objConstants.DefensiveAdditioner;
                     orbwalkingPos.Y = ObjectManager.Player.ServerPosition.Y + objConstants.DefensiveAdditioner;
-                    ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, orbwalkingPos.To3D());
-                    return BehaviorState.Running;
+                    Base.OrbW.ExecuteMixedMode(orbwalkingPos.To3D());
+                    return BehaviorState.Success;
                 }
+
+                Base.OrbW.ExecuteMixedMode(Heroes.Me.ServerPosition);
                 return BehaviorState.Success;
             });
 
