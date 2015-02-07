1e41a4238cb2032c4e6041c7171e71b96f0f15d4
imsosharp <imsosharp@mail.com>
2015-02-07 03:12:54 +0200

    reverted cleanup :worried:

--- Autoplay/Behaviors/Strategy/Positioning/Util.cs
+++ Autoplay/Behaviors/Strategy/Positioning/Util.cs
@@ -90,6 +90,66 @@ namespace AIM.Autoplay.Behaviors.Strategy.Positioning
         }
 
         /// <summary>
+        /// Returns a polygon that contains each position of a team champion
+        /// </summary>
+        /// <param name="allyTeam">returns the polygon for ally team if true, enemy if false</param>
+        /// <returns></returns>
+        internal static Geometry.Polygon GetTeamPolygon(bool allyTeam = true)
+        {
+            var poly = new Geometry.Polygon();
+            foreach (var v2 in allyTeam ? new Util().GetAllyPosList() : new Util().GetEnemyPosList())
+            {
+                poly.Add(v2);
+            }
+            poly.ToClipperPath();
+            return poly;
+        }
+
+        /// <summary>
+        /// Returns a clipper path list of all ally champions
+        /// </summary>
+        public Paths GetAllyPaths()
+        {
+            var allyPaths = new Paths(GetAllyPosList().Count);
+            for (int i = 0; i < GetAllyPosList().Count; i++)
+            {
+                allyPaths[i].Add(new IntPoint(GetAllyPosList().ToArray()[i].X + Randoms.Rand.Next(-150, 150), GetAllyPosList().ToArray()[i].Y + Randoms.Rand.Next(-150, 150)));
+            }
+            return allyPaths;
+        }
+
+        /// <summary>
+        /// returns a clipper paths list of all enemy champion positions
+        /// </summary>
+        public Paths GetEnemyPaths()
+        {
+            var enemyPaths = new Paths(GetEnemyPosList().Count);
+            for (int i = 0; i < GetEnemyPosList().Count; i++)
+            {
+                enemyPaths[i].Add(new IntPoint(GetEnemyPosList().ToArray()[i].X, GetEnemyPosList().ToArray()[i].Y));
+            }
+            return enemyPaths;
+        }
+
+        /// <summary>
+        /// returns a list of all ally positions
+        /// </summary>
+        public List<Vector2> GetAllyPosList()
+        {
+            var allies = ObjectManager.Get<Obj_AI_Hero>().FindAll(h => h.IsAlly && !h.IsMe && !h.IsDead && !h.InFountain()).ToList();
+            return allies.Select(ally => ally.ServerPosition.To2D()).ToList();
+        }
+
+        /// <summary>
+        /// returns a list of all enemy positions
+        /// </summary>
+        public List<Vector2> GetEnemyPosList()
+        {
+            var enemies = ObjectManager.Get<Obj_AI_Hero>().FindAll(h => h.IsEnemy && !h.IsDead && h.IsVisible).ToList();
+            return enemies.Select(enemy => enemy.ServerPosition.To2D()).ToList();
+        }
+
+        /// <summary>
         /// draws a line (c) kortatu
         /// </summary>
         /// <param name="start">line start pos</param>
